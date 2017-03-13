a731b95ff8e2f86f06f0b62c854d62ff107305a8
Guillaume Olivier <guillaume-olivier@laposte.net>
2017-03-13 13:20:50 +0100

    ICA-3: new page competences

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { CompetencesPage } from '../pages/competences/competences';
 
 
 @Component({
@@ -22,7 +22,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'Compétences', component: CompetencesPage }
     ];
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { CompetencesPage } from '../pages/competences/competences';
 
 @NgModule({
   declarations: [
     MyApp,
     Page1,
-    Page2
+    CompetencesPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -17,7 +17,7 @@ import { Page2 } from '../pages/page2/page2';
   entryComponents: [
     MyApp,
     Page1,
-    Page2
+    CompetencesPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
